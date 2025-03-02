5768dbc3159d5cdb22ab1fbc78e820cdd215833a
Theodor Ghezan <theodor@duffel.com>
2025-03-02 08:58:57 +0000

    Workspace tweaks in aerospace
    Signed-off-by: Theodor Ghezan <theodor@duffel.com>

--- darwin/darwin.nix
+++ darwin/darwin.nix
@@ -25,12 +25,42 @@
       enable = true;
 
       settings = {
-        accordion-padding = 0;
+        accordion-padding = 4;
+	on-window-detected = [
+	  {
+	    "if" = {
+	    	app-id = "company.thebrowser.Browser";
+	    };
+	    run = "move-node-to-workspace 7";
+	  }
+          {
+	    "if" = {
+	    	app-id = "com.tinyspeck.slackmacgap";
+	    };
+	    run = "move-node-to-workspace 8";
+	  }
+          {
+	    "if" = {
+	    	app-id = "notion.id";
+	    };
+	    run = "move-node-to-workspace 9";
+	  }
+	];
         on-focused-monitor-changed = [ "move-mouse monitor-lazy-center" ];
         workspace-to-monitor-force-assignment = {
           "1" = ["main"];
           "2" = ["secondary" "main"];
         };
+	gaps = {
+          inner.horizontal = 4;
+          inner.vertical = 4;
+          outer.left = 4;
+          outer.bottom = 4;
+          outer.top = 4;
+          outer.right = 4;
+       };
+
+         
         mode = {
           main = {
             binding = {
@@ -53,9 +83,21 @@
               alt-1 = "workspace 1";
               alt-2 = "workspace 2";
               alt-3 = "workspace 3";
+              alt-4 = "workspace 4";
+              alt-5 = "workspace 5";
+              alt-6 = "workspace 6";
+              alt-7 = "workspace 7";
+              alt-8 = "workspace 8";
+              alt-9 = "workspace 9";
               alt-shift-1 = "move-node-to-workspace 1";
               alt-shift-2 = "move-node-to-workspace 2";
               alt-shift-3 = "move-node-to-workspace 3";
+              alt-shift-4 = "move-node-to-workspace 4";
+              alt-shift-5 = "move-node-to-workspace 5";
+              alt-shift-6 = "move-node-to-workspace 6";
+              alt-shift-7 = "move-node-to-workspace 7";
+              alt-shift-8 = "move-node-to-workspace 8";
+              alt-shift-9 = "move-node-to-workspace 9";
               alt-tab = "workspace-back-and-forth";
               alt-shift-tab = "move-node-to-monitor --wrap-around next";
               alt-shift-semicolon = "mode service";
@@ -75,10 +117,11 @@
 
     jankyborders = {
       enable = true;
-      blur_radius = 5.0;
+      style = "round";
+      width = 3.0;
       hidpi = true;
-      active_color = "0xAAB279A7";
-      inactive_color = "0x33867A74";
+      active_color = "0xffe2e2e3";
+      inactive_color = "0xff414550";
     };
   };
 
@@ -163,6 +206,7 @@
         static-only = true;
         mru-spaces = false;
         show-recents = false;
+	expose-group-apps = true;
       };
       finder = {
         AppleShowAllExtensions = true;
